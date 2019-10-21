3f792bb467f449b6aa4d0c92fecab2b07711afe2
Anand <50521705+arockiaanandraj@users.noreply.github.com>
2019-10-21 23:19:29 +0530

    Added Heroku Support
    Authored-by: Anand <arockiaanandraj@gmail.com>

--- Procfile
+++ Procfile
@@ -1,2 +1,1 @@
-worker: mvn clean install spring-boot:repackage
 web: java -Dserver.port=$PORT $JAVA_OPTS -jar target/das-boot-1.0.jar
--- deploy.bat
+++ deploy.bat
@@ -1,2 +1,3 @@
+mvn clean heroku:deploy
 cf login -a api.run.pivotal.io -u arockiaanandraj@hotmail.com -o pcf-org-cts -s development
 cf push
